4dd46629f257b760fe2f2e1b8425f1d9850f4903
heather <heather.wang@ericsson.com>
2013-09-16 14:54:39 +0800

    add w1337

--- src/com/redarc/lteweb/WebW1333.java
+++ src/com/redarc/lteweb/WebW1333.java
@@ -17,16 +17,24 @@ public class WebW1333 extends BaseWeb{
 	public String body() {
  		Div l3PGRmd = new Div();
  		l3PGRmd.setClass("se_context");
- 		l3PGRmd.addElement(new H1("L3 PG Reminders-W1336"));
+ 		l3PGRmd.addElement(new H1("L3 PG Reminders-W1337"));
  		
- 		l3PGRmd.addElement(new H2("L13B GA"));
- 		l3PGRmd.addElement(new p().setTagText("&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;L13B FDD and TDD have reached GA status as planned today September 5,2013 "));
+ 		l3PGRmd.addElement(new H2("Removal of LDN in RAC ,avoid using LDN in RAC"));
+ 		l3PGRmd.addElement(new p().setTagText("&nbsp;&nbsp;&nbsp;&nbsp;LDN in signals is replaced with FroRef,parentRef"));
+ 		l3PGRmd.addElement(new p().setTagText("&nbsp;&nbsp;&nbsp;&nbsp;LDN removed in most RNH and UEH code,should not be used in cellLM, mostly removed in centralLM (still used in some OMF blocks)"));
+ 		//l3PGRmd.addElement(new p().setTagText("&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;L13B FDD and TDD have reached GA status as planned today September 5,2013 "));
  		
- 		l3PGRmd.addElement(new H2("RAST Alternative is ready for use"));
- 		l3PGRmd.addElement(new p().setTagText("&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;RAST Alternative running is same as RAST just with different config file under '/proj/lterbsSwdi/usr/lterbsnj'."));
+ 		l3PGRmd.addElement(new H2("Delivery request rules"));
+ 		l3PGRmd.addElement(new p().setTagText("&nbsp;&nbsp;&nbsp;&nbsp;FOR WPs"));
+ 		l3PGRmd.addElement(new p().setTagText("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Always email TO:LTE RBS Integration Delivery Request (EMC)"));
+ 		l3PGRmd.addElement(new p().setTagText("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;UEH area: still to mail group UEH Delivery Coordination"));
  		
- 		l3PGRmd.addElement(new H2("L14A is open, SAM/C&O are OK to deliver"));
- 		l3PGRmd.addElement(new p().setTagText("&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;Link reference click here"));
+ 		
+ 		l3PGRmd.addElement(new p().setTagText("&nbsp;&nbsp;&nbsp;&nbsp;FOR TRs"));
+ 		l3PGRmd.addElement(new p().setTagText("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;14A RNH TRs are allowed to be delivered without sending delivery requests (except UEH)! But not if there is a complete delivery stop"));
+ 		l3PGRmd.addElement(new p().setTagText("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;14A UEH TRs need to send delivery request to UEH Delivery Cooridnation"));
+ 		l3PGRmd.addElement(new p().setTagText("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;13A TRs need to propose an answer in MHWeb and get approval from mDPM ; 13B TRs deliver to IP2 after checking with Kim Augustsson"));
+ 		//l3PGRmd.addElement(new p().setTagText("&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;Link reference click here"));
  		
  		
  		Div wholePage = new Div();
